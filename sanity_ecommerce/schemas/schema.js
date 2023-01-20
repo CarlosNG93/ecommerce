e67f66557f83d172045a984684f1b392cf7b7a66
Carlos <carlosng1993@gmail.com>
2023-01-20 11:29:16 +0100

    deat: assets and components added

--- sanity_ecommerce/schemas/schema.js
+++ sanity_ecommerce/schemas/schema.js
@@ -1,4 +1,4 @@
-import createSchema from 'part:@sanity/base/schema-creator';
+/* import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 import product from './product';
@@ -7,4 +7,4 @@ import banner from './banner';
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([ product, banner ]),
-})
+}) */
